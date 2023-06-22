55f0fdaa304f294b7e688cec5af70d70a34cdfd9
GabrielBifano <gabrielfev@outlook.com>
2023-06-22 14:09:49 -0300

    i think this setLayers work

--- nodes/nodeIn.dart
+++ nodes/nodeIn.dart
@@ -2,6 +2,8 @@ import 'node.dart';
 
 class NodeInput extends Node {
 
+  NodeInput.set(next, nextWB): super.set(next, nextWB);
+
   @override
   void call(input, weight, bias) {
     for(int i = 0; i < next.length; i++){
